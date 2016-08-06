db3c57a346867844d69af8618b83db48af869d47
Brandon Loudermilk <b.c.loudermilk@gmail.com>
2016-08-06 10:56:53 -0500

    ch-4

---
+++ ch-4.R
@@ -0,0 +1,28 @@
+## ch-4.R
+## exercises from chap 4 of dog bayes
+
+show(HairEyeColor)
+dim(HairEyeColor)
+?apply
+EyeHairFreq = apply(HairEyeColor, c("Eye", "Hair"), sum)
+EyeHairFreq
+(EyeHairProp = EyeHairFreq/sum(EyeHairFreq))
+show(round(EyeHairProp, 2))
+(HairFreq = apply(EyeHairFreq, c("Hair"), sum))
+HairProp = HairFreq/sum(HairFreq)
+HairProp
+show(round(HairProp, 2))
+(EyeFreq = apply(HairEyeColor, c("Eye"), sum))
+EyeProp = EyeFreq/sum(EyeFreq)
+show(round(EyeProp, 2))
+EyeHairProp["Blue",]/EyeProp["Blue"]
+
+
+## 4.3 What is prob of drawing a 10 from pinochle deck
+## dech = 48 cards
+vals <- c("9", "10", "J", "Q", "K", "A")
+vals <- c(vals, vals)
+suits <- c("heart", "diamond", "club", "spade")
+cards <- expand.grid(value = vals, suits = suits)
+t <- table(cards)
+s
